7121e8d22a1f14e95a4a1f685cb6b0bcabe6c349
Agurato <Agurato@users.noreply.github.com>
2022-10-10 12:24:39 +0200

    debug actions

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -43,7 +43,7 @@ func CacheFile(sourceUrl string, filePath string) (hasToWait bool, err error) {
 	// Create directories in the requested path if needed
 	parent := GetCachedPath(filepath.Dir(filePath))
 	fmt.Println("parent", parent)
-	outTest, err := exec.Command("ls", "../../cache").Output()
+	outTest, err := exec.Command("ls", "-R", "../../../").Output()
 	fmt.Println(string(outTest), err)
 	if _, err := os.Stat(parent); errors.Is(err, os.ErrNotExist) {
 		err = os.MkdirAll(parent, os.ModeDir)
